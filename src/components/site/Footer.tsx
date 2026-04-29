27e76bc9e5d1e1ffe13569273de56f4bedf55a9d
mihneaserban <162312685+mihneaserban@users.noreply.github.com>
2026-04-29 17:35:50 +0300

    editare footer - v5

--- src/components/site/Footer.tsx
+++ src/components/site/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
   return (
     <footer
       // Am adăugat block, mb-[-1px] și pb-10 aici
-      className="relative block pt-20 pb-10 px-6 text-white isolate overflow-hidden mb-[-100px] pb-[140px]"
+      className="relative block pt-20 pb-10 px-6 text-white isolate overflow-hidden mb-[-80px] pb-[110px]"
       style={{ 
         background: "var(--gradient-bg-dark)",
         marginTop: "0",
